04276e7ea533e3cf6a0d28ce7c240b16cc7fb293
ismail <emcpalnd@gmail.com>
2016-02-11 00:52:32 +0200

    ff

--- app/src/main/java/com/pcland15/ismail/gtc_t_app/libs/config.java
+++ app/src/main/java/com/pcland15/ismail/gtc_t_app/libs/config.java
@@ -4,9 +4,18 @@ package com.pcland15.ismail.gtc_t_app.libs;
  * Created by empcl_000 on 19/10/2015.
  */
 public class config {
+    public  static  String  webServiecURL="http://tabukasl.com/webserves/";
+    public  static  String  imagePath="http://tabukasl.com/upload/";
+    public  static  String  vidoePath="http://tabukasl.com/upload/";
+
+
+
+    /*
+
+
     public  static  String  webServiecURL="http://gtc.decorplanner.com/webserves/";
     public  static  String  imagePath="http://gtc.decorplanner.com/upload/";
     public  static  String  vidoePath="http://gtc.decorplanner.com/upload/";
-
+     */
 
 }
